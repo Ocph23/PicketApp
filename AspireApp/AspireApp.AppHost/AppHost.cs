9ad9df750aa053a9a30edbb77b1362895a35fbfa
ocph23 <ocph23@gmail.com>
2025-10-11 22:58:53 +0900

    add aspire

--- AspireApp/AspireApp.AppHost/AppHost.cs
+++ AspireApp/AspireApp.AppHost/AppHost.cs
@@ -8,28 +8,11 @@ var kursusdb = postgres.AddDatabase("piketdb");
 
 var redis = builder.AddRedis("Redis");
 
-//var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
-//    .WithHttpHealthCheck("/health");
-
-//builder.AddProject<Projects.AspireApp_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithHttpHealthCheck("/health")
-//    .WithReference(apiService)
-//    .WaitFor(apiService);
-
-
-
-
-
 var picketapi = builder.AddProject<Projects.PiketWebApi>("piketapi")
     .WithReference(redis)
     .WaitFor(postgres)
      .WithHttpEndpoint(name: "rest", port: 5001, isProxied: false)
-    //.WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints();
-//.WithHttpHealthCheck("/health");
-
-
 
 builder.AddNpmApp("vue", "../../smkn8picket-client")
     .WithReference(picketapi)
